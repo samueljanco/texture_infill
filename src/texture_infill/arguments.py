4aa6912c69c5be329d99d7d32a12428a309aca0b
samueljanco <124911218+samueljanco@users.noreply.github.com>
2026-07-11 16:27:05 +0200

    Add files via upload

---
+++ src/texture_infill/arguments.py
@@ -0,0 +1,72 @@
+from dataclasses import dataclass
+
+Color = tuple[int, int, int]
+
+@dataclass
+class Arguments:
+
+    # texture
+    DENSITY_APPLICABLE_TEXTURES = ["texture_D", "texture_G", "texture_I", "texture_J"]
+    texture_name: str
+    density_applicable: bool
+
+    # texture modifications
+    count: int
+    scale_x: float
+    scale_y: float
+    density_x: float
+    density_y: float
+    rotation: float
+    global_scale: float
+
+    # colors
+    colors: list[Color]
+    color_strengths: list[float]
+    white_background: bool
+    background_brightness: float
+
+    # square texture
+    generate_square: bool
+    width: int
+    height: int
+
+    seed: int
+
+    def __init__(self,
+                 texture_name: str = "texture_J",
+                 count: int = 1,
+                 scale_x: float = 1.0,
+                 scale_y: float = 1.0,
+                 density_x: float = 0.0,
+                 density_y: float = 0.0,
+                 rotation: float = 0.0,
+                 global_scale: float = 1.0,
+                 colors: list[Color] = [(0, 0, 0)],
+                 color_strengths: list[float] = [1.0],
+                 white_background: bool = True,
+                 background_brightness: float = 1.0,
+                 generate_square: bool = False,
+                 width: int = 1024,
+                 height: int = 1024,
+                 seed: int = 42
+                 ):
+
+        self.texture_name = texture_name
+        self.density_applicable = self.texture_name in self.DENSITY_APPLICABLE_TEXTURES
+        self.count = count
+        self.scale_x = scale_x
+        self.scale_y = scale_y
+        self.density_x = density_x
+        self.density_y = density_y
+        self.rotation = rotation
+        self.global_scale = global_scale
+        self.colors = colors
+        self.color_strengths = color_strengths
+        self.white_background = white_background
+        self.background_brightness = background_brightness
+        self.generate_square = generate_square
+        self.width = width
+        self.height = height
+        self.seed = seed
+
+
